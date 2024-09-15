f9c488fce46bd25e0dd3f074756a5723bb6f69e3
Abdelhadi Oumar <abdelhadioumar@gmail.com>
2024-09-15 23:40:33 +0100

    Fix user-info-box design

--- index.js
+++ index.js
@@ -39,41 +39,43 @@ toggleButtonState();
 
 //get user info
 function getUserInfo(username) {
-  fetch(`https://api.github.com/users/${username}`)
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.message === "Not Found") {
-        alert("User not found");
-        return;
-      }
-      // Clear any existing user info
-      document.getElementsByClassName("user-box")[0].innerHTML = "";
-      const userInfoBox = document.querySelector(".user-box");
+    fetch(`https://api.github.com/users/${username}`)
+        .then((response) => response.json())
+        .then((data) => {
+        if (data.message === "Not Found") {
+            alert("User not found");
+            return;
+        }
+        // Clear any existing user info
+        // document.getElementsByClassName("user-box")[0].innerHTML = "";
+        const userInfoBox = document.createElement("div");
+        userInfoBox.classList.add("user-box");
+        // const userInfoBox = document.querySelector(".user-box");
 
-      // Create user info div
-      const userInfoDiv = document.createElement("div");
-      userInfoDiv.classList.add("user-info");
+        // Create user info div
+        const userInfoDiv = document.createElement("div");
+        userInfoDiv.classList.add("user-info");
 
-      // Create profile section
-      const profileDiv = document.createElement("div");
-      profileDiv.classList.add("profile");
+        // Create profile section
+        const profileDiv = document.createElement("div");
+        profileDiv.classList.add("profile");
 
-      // Create avatar section
-      const avatarDiv = document.createElement("div");
-      avatarDiv.classList.add("avatar-section");
+        // Create avatar section
+        const avatarDiv = document.createElement("div");
+        avatarDiv.classList.add("avatar-section");
 
-      const avatar = document.createElement("img");
-      avatar.classList.add("avatar");
-      avatar.src = data.avatar_url;
-      avatar.alt = "User Avatar";
-      avatarDiv.appendChild(avatar);
+        const avatar = document.createElement("img");
+        avatar.classList.add("avatar");
+        avatar.src = data.avatar_url;
+        avatar.alt = "User Avatar";
+        avatarDiv.appendChild(avatar);
 
-      const userDetails = document.createElement("div");
-      userDetails.classList.add("user-details");
+        const userDetails = document.createElement("div");
+        userDetails.classList.add("user-details");
 
-      const name = document.createElement("h2");
-      name.classList.add("name");
-      name.textContent = data.name || "No Name";
+        const name = document.createElement("h2");
+        name.classList.add("name");
+        name.textContent = data.name || "No Name";
 
         const usernamePara = document.createElement('a');
         usernamePara.classList.add('username');
@@ -81,87 +83,97 @@ function getUserInfo(username) {
         usernamePara.target = '_blank';
         usernamePara.textContent = `@${data.login}`;
 
-      const bio = document.createElement("p");
-      bio.classList.add("bio");
-      bio.textContent = data.bio || "This profile has no bio";
+        const bio = document.createElement("p");
+        bio.classList.add("bio");
+        bio.textContent = data.bio || "This profile has no bio";
 
-      const joinedDate = document.createElement("p");
-      joinedDate.classList.add("joined-date");
-      joinedDate.textContent = `Joined ${new Date(
-        data.created_at
-      ).toDateString()}`;
+        const joinedDate = document.createElement("p");
+        joinedDate.classList.add("joined-date");
+        joinedDate.textContent = `Joined ${new Date(
+            data.created_at
+        ).toDateString()}`;
 
-      // Append name and date joined together
-      const nameAndDate = document.createElement("div");
-      nameAndDate.classList.add("name-date-box");
-      nameAndDate.appendChild(name);
-      nameAndDate.appendChild(joinedDate);
+        // Append name and date joined together
+        const nameAndDate = document.createElement("div");
+        nameAndDate.classList.add("name-date-box");
+        nameAndDate.appendChild(name);
+        nameAndDate.appendChild(joinedDate);
 
-      // Append user details to profile section
-      userDetails.appendChild(nameAndDate);
-      userDetails.appendChild(usernamePara);
-      userDetails.appendChild(bio);
+        // Append user details to profile section
+        userDetails.appendChild(nameAndDate);
+        userDetails.appendChild(usernamePara);
+        userDetails.appendChild(bio);
 
-      // profileDiv.appendChild(avatar);
-      profileDiv.appendChild(userDetails);
+        // profileDiv.appendChild(avatar);
+        profileDiv.appendChild(userDetails);
 
-      // Create stats section
-      const statsDiv = document.createElement("div");
-      statsDiv.classList.add("stats");
+        // Create stats section
+        const statsDiv = document.createElement("div");
+        statsDiv.classList.add("stats");
 
-      const statRepos = createStatItem("Repos", data.public_repos);
-      const statFollowers = createStatItem("Followers", data.followers);
-      const statFollowing = createStatItem("Following", data.following);
+        const statRepos = createStatItem("Repos", data.public_repos);
+        const statFollowers = createStatItem("Followers", data.followers);
+        const statFollowing = createStatItem("Following", data.following);
 
-      statsDiv.appendChild(statRepos);
-      statsDiv.appendChild(statFollowers);
-      statsDiv.appendChild(statFollowing);
+        statsDiv.appendChild(statRepos);
+        statsDiv.appendChild(statFollowers);
+        statsDiv.appendChild(statFollowing);
 
-      // Create additional info section
-      const additionalInfoDiv = document.createElement("div");
-      additionalInfoDiv.classList.add("additional-info");
+        // Create additional info section
+        const additionalInfoDiv = document.createElement("div");
+        additionalInfoDiv.classList.add("additional-info");
 
-      const location = createInfoItem(
-        data.location || "Not Available",
-        "location"
-      );
-      const blog = createInfoItem(data.blog || "Not Available", "blog");
-      const twitter = createInfoItem(
-        data.twitter_username ? `@${data.twitter_username}` : "Not Available",
-        "twitter"
-      );
-      const company = createInfoItem(
-        data.company || "Not Available",
-        "company"
-      );
+        const location = createInfoItem(
+            data.location || "Not Available",
+            "location"
+        );
+        const blog = createInfoItem(data.blog || "Not Available", "blog");
+        const twitter = createInfoItem(
+            data.twitter_username ? `@${data.twitter_username}` : "Not Available",
+            "twitter"
+        );
+        const company = createInfoItem(
+            data.company || "Not Available",
+            "company"
+        );
 
-      const locationXBox = document.createElement("div");
-      const blogCompanyBox = document.createElement("div");
-      locationXBox.classList.add("additional-info-box");
-      blogCompanyBox.classList.add("additional-info-box");
+        const locationXBox = document.createElement("div");
+        const blogCompanyBox = document.createElement("div");
+        locationXBox.classList.add("additional-info-box");
+        blogCompanyBox.classList.add("additional-info-box");
 
-      locationXBox.appendChild(location);
-      locationXBox.appendChild(twitter);
+        locationXBox.appendChild(location);
+        locationXBox.appendChild(twitter);
 
-      blogCompanyBox.appendChild(blog);
-      blogCompanyBox.appendChild(company);
+        blogCompanyBox.appendChild(blog);
+        blogCompanyBox.appendChild(company);
 
-      additionalInfoDiv.appendChild(locationXBox);
-      additionalInfoDiv.appendChild(blogCompanyBox);
+        additionalInfoDiv.appendChild(locationXBox);
+        additionalInfoDiv.appendChild(blogCompanyBox);
 
-      // Append all section to the user info div
-      userInfoDiv.appendChild(profileDiv);
-      userInfoDiv.appendChild(statsDiv);
-      userInfoDiv.appendChild(additionalInfoDiv);
+        // Append all section to the user info div
+        userInfoDiv.appendChild(profileDiv);
+        userInfoDiv.appendChild(statsDiv);
+        userInfoDiv.appendChild(additionalInfoDiv);
 
-      // Append all sections to the user info box
-      userInfoBox.appendChild(avatarDiv);
-      userInfoBox.appendChild(userInfoDiv);
-
-      // Append the user info box to the container
-      // document.getElementById('user-info-container').appendChild(userInfoBox);
-    })
-    .catch((error) => console.error("Error:", error));
+        // Append all sections to the user info box
+        userInfoBox.appendChild(avatarDiv);
+        userInfoBox.appendChild(userInfoDiv);
+        // clear any existing user info
+            // document.getElementsByClassName("user-box")[0].innerHTML = "";
+        if (document.getElementsByClassName("user-box")[0]) {
+            // delete all existing divs with class name "user-box", there might be more than one
+            const userBoxes = document.getElementsByClassName("user-box");
+            for (let i = 0; i < userBoxes.length; i++) {
+                userBoxes[i].remove();
+            }
+        }
+                    
+        // Append the user info box to the body
+        document.body.appendChild(userInfoBox);
+        // document.getElementById('user-info-container').appendChild(userInfoBox);
+        })
+        .catch((error) => console.error("Error:", error));
 }
 
 // Helper functions to create reusable DOM elements
@@ -194,6 +206,7 @@ function createInfoItem(content, type) {
 
     const href = type === "location" || type === "company" ? null : type === "blog" ? content : `https://twitter.com/${content}`;
     const infoItem = getInfoItemElement(type, content, href);
+
     if (href) {
         infoItem.classList.add("link");
     }
